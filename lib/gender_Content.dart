3bb65826c25d6ee636b82e7a710494a42f6f16a2
anujwar <anujrw18@gmail.com>
2022-04-05 14:06:13 +0530

    first commit

---
+++ lib/gender_Content.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+
+class GenderContent extends StatelessWidget {
+  const GenderContent(
+      {Key? key,
+      required this.icon,
+      required this.gender,
+      required this.active})
+      : super(key: key);
+
+  final IconData icon;
+  final String gender;
+  final bool active;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          size: 40,
+          color: active ? Colors.white : Colors.blueGrey,
+        ),
+        Text(
+          gender,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: active ? Colors.white : Colors.blueGrey,
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: unnecessary_const, unused_import, prefer_const_constructors
+
+import 'gender_Content.dart';
+import 'measure.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedGender = 1;
+  int height = 160;
+  int weight = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "BMI CALCULATOR",
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //gender
+          Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => setState(() {
+                    selectedGender = 1;
+                  }),
+                  child: Container(
+                      padding: EdgeInsets.all(30),
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.blue,
+                      ),
+                      child: GenderContent(
+                        icon: Icons.male,
+                        gender: "Male",
+                        active: (selectedGender == 1),
+                      )),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => setState(() {
+                    selectedGender = 0;
+                  }),
+                  child: Container(
+                      padding: EdgeInsets.all(30),
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Colors.blue,
+                      ),
+                      child: GenderContent(
+                        icon: Icons.female,
+                        gender: "Female",
+                        active: (selectedGender == 0),
+                      )),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+              child: Image.network(
+            'https://media.giphy.com/media/btq3qTOq0pT8Y/giphy.gif',
+            height: 400,
+            fit: BoxFit.contain,
+          )),
+          const Measure(),
+          //weight and height
+          //Calculate_Button(),
+        ],
+      ),
+    );
+  }
+}
